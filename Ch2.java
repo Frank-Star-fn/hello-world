877a6ff588731dc01de6be1b517f620c17a70466
Frank-Star-fn <72490620+Frank-Star-fn@users.noreply.github.com>
2022-02-05 12:12:41 +0800

    Add files via upload
    hello world by Java

---
+++ Ch2.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+import java.awt.*;
+import java.applet.*;
+import javax.swing.*;
+
+public class Ch2 extends JApplet {
+	public void paint(Graphics g){ 
+		g.drawString("Hello World!", 20, 20);
+	}
+}
